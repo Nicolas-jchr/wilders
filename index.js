ba50474a0cade95b5a3e34ffc3e3992492cf74bc
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-03-19 10:40:06 +0100

    Add colours on Wilders

---
+++ index.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+
+console.log(
+    chalk.red("Max la menace des clickers"))
+console.log(
+    chalk.green("Nico G el goat del todo de Nico"))
+console.log(
+    chalk.blue("Antho le BG"))
+console.log(
+    chalk.yellow("Benoît le GOAT suprême")
+)
+
